bfc2cbdd09c35dac082d26de259dba27f9a37bae
amrgharz <amr.gharz@gmail.com>
2018-08-26 21:50:28 +0300

    start designing the dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,7 +7,64 @@
 
 		include 'init.php';
 
-		
+		// Start The Dashboard Page
+?>
+		<div class='container home-stats text-center'>
+			<h1>Dashboard</h1>
+			<div class='row'>
+				<div class='col-md-3'>
+					<div class='stat'>
+						Total Mambers
+						<span>200</span>
+					</div>	
+				</div>
+				<div class='col-md-3'>
+					<div class='stat'>
+						Pending Mambers
+						<span>25</span>
+					</div>	
+				</div>
+				<div class='col-md-3'>
+					<div class='stat'>
+						Total Items
+						<span>1500</span>
+					</div>	
+				</div>
+				<div class='col-md-3'>
+					<div class='stat'>
+						Total Comments
+						<span>200</span>
+					</div>	
+				</div>
+			</div>
+		</div>
+
+		<div class='container latest'>
+			<div class = 'row'>
+				<div class = col-sm-6>
+					<div class = 'panel panel-default'>
+						<div class='panel-heading'>
+							<i class='fa fa-users'></i>Latest Users
+						</div>
+					</div>
+					<div class = 'panel-body'>
+						Test
+					</div>
+				</div>
+				<div class = col-sm-6>
+					<div class = 'panel panel-default'>
+						<div class='panel-heading'>
+							<i class='fa fa-tag'></i>Latest Items
+						</div>
+					</div>
+					<div class = 'panel-body'>
+						Test
+					</div>
+				</div>
+			</div>
+		</div>
+<?php
+		// End of Dashboard Page
 
 		include $tpl . 'footer.php';
 
